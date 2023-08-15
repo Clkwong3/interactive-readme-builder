90f0ae8162d008419d1db568a1e5dabf9878c133
Clkwong3 <134983608+Clkwong3@users.noreply.github.com>
2023-08-15 01:35:41 -0700

    add the sample folder to put the sample readme in const filePath

--- index.js
+++ index.js
@@ -4,88 +4,87 @@ const path = require("path"); // the path module
 const generateReadMe = require("./generateReadMe");
 
 console.log("ReadMe Generator is running");
+// The questions
+const questions = [
+  {
+    type: "input",
+    name: "title",
+    message: "What is the title of the project?",
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "Why is this project made? What does it do?",
+  },
+  {
+    // ask if user wants a table of contents
+    type: "confirm",
+    name: "includeTableOfContents",
+    message: "Want to include a table of contents?",
+    default: true,
+  },
+  {
+    type: "input",
+    name: "installation",
+    message: "What do users need to install this application?",
+  },
+  {
+    type: "input",
+    name: "usage",
+    message: "What are the instructions for using this application?",
+  },
+  {
+    type: "list",
+    name: "license",
+    message: "Select the license being used for this project.",
+    choices: [
+      "Apache 2.0",
+      "GNU GPL 3.0",
+      "MIT",
+      "BSD 2",
+      "BSD 3",
+      "Boost 1.0",
+      "CCZ 1.0",
+      "EPL 2",
+      "GNU AGPL 3.0",
+      "GNU LGPL 2.1",
+      "Mozilla 2.0",
+      "Unlicense",
+      "N/A",
+    ],
+  },
+  {
+    type: "input",
+    name: "credits",
+    message: "Any contributors to this project?",
+  },
+  {
+    type: "input",
+    name: "test",
+    message: "How can users test this application?",
+  },
+  {
+    // ask if user wants a table of contents
+    type: "confirm",
+    name: "includeContactInfo",
+    message: "Want to add contact information?",
+    default: true,
+  },
+  {
+    type: "input",
+    name: "github",
+    message: "GitHub Account Name:",
+    when: (answers) => answers.includeContactInfo, // should only appear when users type 'y'
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "Email Address:",
+    when: (answers) => answers.includeContactInfo, // should only appear when users type 'y'
+  },
+];
 
-function startPrompts() {
-  // The questions
-  const questions = [
-    {
-      type: "input",
-      name: "title",
-      message: "What is the title of the project?",
-    },
-    {
-      type: "input",
-      name: "description",
-      message: "Why is this project made? What does it do?",
-    },
-    {
-      // ask if user wants a table of contents
-      type: "confirm",
-      name: "includeTableOfContents",
-      message: "Want to include a table of contents?",
-      default: true,
-    },
-    {
-      type: "input",
-      name: "installation",
-      message: "What do users need to install this application?",
-    },
-    {
-      type: "input",
-      name: "usage",
-      message: "What are the instructions for using this application?",
-    },
-    {
-      type: "list",
-      name: "license",
-      message: "Select the license being used for this project.",
-      choices: [
-        "Apache 2.0",
-        "GNU GPL 3.0",
-        "MIT",
-        "BSD 2",
-        "BSD 3",
-        "Boost 1.0",
-        "CCZ 1.0",
-        "EPL 2",
-        "GNU AGPL 3.0",
-        "GNU LGPL 2.1",
-        "Mozilla 2.0",
-        "Unlicense",
-        "N/A",
-      ],
-    },
-    {
-      type: "input",
-      name: "credits",
-      message: "Any contributors to this project?",
-    },
-    {
-      type: "input",
-      name: "test",
-      message: "How can users test this application?",
-    },
-    {
-      // ask if user wants a table of contents
-      type: "confirm",
-      name: "includeContactInfo",
-      message: "Want to add contact information?",
-      default: true,
-    },
-    {
-      type: "input",
-      name: "github",
-      message: "GitHub Account Name:",
-      when: (answers) => answers.includeContactInfo, // should only appear when users type 'y'
-    },
-    {
-      type: "input",
-      name: "email",
-      message: "Email Address:",
-      when: (answers) => answers.includeContactInfo, // should only appear when users type 'y'
-    },
-  ];
-
+function init() {
   // Ask and gather
   inquirer
     .prompt(questions)
@@ -94,7 +93,7 @@ function startPrompts() {
 
       // Use path.join to specify where the README.md should be saved
       // process.cwd() turns the current working directory as a string
-      const filePath = path.join(process.cwd(), "README.md");
+      const filePath = path.join(process.cwd(), "sample", "README.md");
 
       // Write the generated README content to a file
       fs.writeFile(filePath, readmeContent, (err) => {
@@ -110,4 +109,4 @@ function startPrompts() {
     });
 }
 // reset and start the prompts
-startPrompts();
+init();
