97192c6a9bedf2725d6622d2f96f63cc5d43b8c4
Clkwong3 <134983608+Clkwong3@users.noreply.github.com>
2023-08-19 00:01:26 -0700

    split string into arrays using (,). map() performs item.trim() to remove whitespace

--- generateMarkDown.js
+++ generateMarkDown.js
@@ -56,6 +56,9 @@ function generateMarkDown(answers) {
 
   // Contributors
   if (answers.includeCredits && answers.contributors) {
+    const contributorDetails = answers.contributors
+      .split(",")
+      .map((item) => item.trim());
   }
 
   // Contact Me
